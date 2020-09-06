664d77e7ead578f08dc3fba20388b144e56eb8e3
unknown <550909227@qq.com>
2020-09-06 20:10:57 +0800

    add entity about define

---
+++ Re-Metal-Max/Assets/Scripts/Logic/Entity/PlayerEntity/PlayerHumanController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using ReMetalMax.Core;
+using ReMetalMax.Core.Event;
+
+namespace ReMetalMax.Logic.PlayerEntity
+{
+    class PlayerHumanController : IController
+    {
+        private EventContext m_context;
+        private IEntity m_currentEntity;
+
+        public void Attach(EventContext context, IEntity entity, ISource source = null) {
+            OnAttach();
+            m_context = context;
+            m_currentEntity = entity;
+            if (source != null) {
+                m_currentEntity.EntitySource = source;
+            }
+        }
+        public void OnAttach() { 
+        
+        }
+
+        public void OnDeattach() { 
+        
+        }
+    }
+}
