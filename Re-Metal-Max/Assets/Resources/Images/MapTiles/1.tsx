d25d35f65e6bfa13b76488b3ddeaec008b83a4a7
Huisama <yuwenhusiama@outlook.com>
2020-09-14 23:26:08 +0800

    Update OGMO

--- Re-Metal-Max/Assets/Resources/Images/MapTiles/1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="1" tilewidth="32" tileheight="32" tilecount="1512" columns="8">
- <image source="1.png" width="282" height="6048"/>
-</tileset>
--- Re-Metal-Max/Assets/Resources/Images/MapTiles/2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="2" tilewidth="32" tileheight="32" tilecount="144" columns="8">
- <image source="2.png" width="256" height="600"/>
-</tileset>
